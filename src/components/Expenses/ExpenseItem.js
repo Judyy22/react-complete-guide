1558cbcca7f84168af363ea777376d2efc7e5458
Judyy22 <115631038+Judyy22@users.noreply.github.com>
2023-02-15 22:46:57 +0900

    update filter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,27 +2,19 @@ import "./ExpenseItem.css";
 import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import ExpensesFilter from "./ExpensesFilter";
 
 const ExpenseItem = (props) => {
     const [title, setTitle] = useState(props.title);
     // console.log("ExpenseItem evaluated by React");
 
-    const clickHandler = () => {
-        setTitle("Updated!");
-        console.log(title);
-    };
-
     return (
         <div>
-            <ExpensesFilter />
             <Card className="expense-item">
                 <ExpenseDate date={props.date} />
                 <div className="expense-item__description">
-                    <h2>{title}</h2>
+                    <h2>{props.title}</h2>
                     <div className="expense-item__price">${props.amount}</div>
                 </div>
-                <button onClick={clickHandler}>Change Title</button>
             </Card>
         </div>
     );
